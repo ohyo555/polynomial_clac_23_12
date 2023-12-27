37fb828e769b8d11c64b026aa018b4d43c9a147e
ohyo555 <555gywn@gmail.com>
2023-12-27 11:46:15 +0900

    10 * 10 * 10

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -2,6 +2,7 @@ package com.ll;
 
 public class Main {
   public static void main(String[] args) {
-
+    System.out.println("10 * -10");
+    System.out.printf("10 * -10 = %d", 10*-10);
   }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -61,14 +61,32 @@ public class CalcTest {
   }
 
   @Test
-  @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+  @DisplayName("10 - 10 - 10 - 10 == -20")
   void t10() {
+    assertThat(Calc.run("10 - 10 - 10 - 10")).isEqualTo(-20);
+  }
+
+  @Test
+  @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+  void t11() {
     assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
   }
 
   @Test
-  @DisplayName("10 - 10 - 10 - 10 == -20")
-  void t11() {
-    assertThat(Calc.run("10 - 10 - 10 - 10")).isEqualTo(-20);
+  @DisplayName("10 * 10 == 100")
+  void t12() {
+    assertThat(Calc.run("10 * 10")).isEqualTo(100);
+  }
+
+  @Test
+  @DisplayName("10 * -10 == -100")
+  void t13() {
+    assertThat(Calc.run("10 * -10")).isEqualTo(-100);
+  }
+
+  @Test
+  @DisplayName("10 * 10 * 10 == 1000")
+  void t14() {
+    assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
   }
 }
