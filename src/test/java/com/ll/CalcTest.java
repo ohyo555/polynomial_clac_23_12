d971dff79b7f1f5b880d13de54fb4cc856a18f74
ohyo555 <555gywn@gmail.com>
2023-12-28 08:58:03 +0900

    (20 + 20) + 20 == 60

--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -103,8 +103,39 @@ public class CalcTest {
   }
 
   @Test
-  @DisplayName("(((10 + 20))) == 30")
+  @DisplayName("((10 + 20)) == 30")
   void t17() {
+    assertThat(Calc.run("((10 + 20))")).isEqualTo(30);
+  }
+
+  @Test
+  @DisplayName("(((10 + 20))) == 30")
+  void t18() {
     assertThat(Calc.run("(((10 + 20)))")).isEqualTo(30);
   }
+
+  @Test
+  @DisplayName("100 == 100")
+  void t19() {
+    assertThat(Calc.run("100")).isEqualTo(100);
+  }
+
+  @Test
+  @DisplayName("(100) == 100")
+  void t20() {
+    assertThat(Calc.run("(100)")).isEqualTo(100);
+  }
+
+  @Test
+  @DisplayName("(100)  == 100")
+  void t21() {
+    assertThat(Calc.run("(100) ")).isEqualTo(100);
+  }
+
+  @Test
+  @DisplayName("(20 + 20) + 20 == 60")
+  void t22() {
+    assertThat(Calc.run("(20 + 20) + 20")).isEqualTo(60);
+  }
+
 }
